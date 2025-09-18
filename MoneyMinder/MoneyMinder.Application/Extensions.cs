ec8b8d6affe62a199761319babf30b611c371707
KamilPachuta <pachutakamil@gmail.com>
2025-09-18 22:19:40 +0200

    fix(logging): Fixed Serilog configuration

--- MoneyMinder/MoneyMinder.Application/Extensions.cs
+++ MoneyMinder/MoneyMinder.Application/Extensions.cs
@@ -12,8 +12,6 @@ public static class Extensions
     {
         services.AddScoped<IPasswordHasher<Account>, PasswordHasher<Account>>();
 
-        services.AddSerilog();
-
         return services;
     }
     
