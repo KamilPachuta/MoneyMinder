9dcfed877ee5f59b65d061812656daf973742a48
KamilPachuta <pachutakamil@gmail.com>
2025-09-24 18:12:00 +0200

    feat(CurrencyAccounts): add InvalidBudgetDateException for domain validation

---
+++ MoneyMinder/MoneyMinder.Domain/CurrencyAccounts/Exceptions/InvalidBudgetDateException.cs
@@ -0,0 +1,6 @@
+﻿using MoneyMinder.Domain.Shared.Abstractions;
+
+namespace MoneyMinder.Domain.CurrencyAccounts.Exceptions;
+
+internal sealed class InvalidBudgetDateException(DateTime date) 
+    : MoneyMinderException($"Invalid budget date: '{date}'. The budget date must be for the current month.");
