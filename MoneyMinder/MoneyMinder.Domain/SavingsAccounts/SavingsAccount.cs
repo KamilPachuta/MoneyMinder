ce44751b51a8e55916db987925c38432795717f0
KamilPachuta <pachutakamil@gmail.com>
2025-09-24 21:36:31 +0200

    feat(SavingsAccounts): add SavingsAccount aggregate to domain model

---
+++ MoneyMinder/MoneyMinder.Domain/SavingsAccounts/SavingsAccount.cs
@@ -0,0 +1,13 @@
+﻿using MoneyMinder.Domain.Accounts;
+using MoneyMinder.Domain.Shared.Primitives;
+
+namespace MoneyMinder.Domain.SavingsAccounts;
+
+public class SavingsAccount : AggregateRoot
+{
+    
+    
+    public Account Account { get; init; }
+    
+    
+}
