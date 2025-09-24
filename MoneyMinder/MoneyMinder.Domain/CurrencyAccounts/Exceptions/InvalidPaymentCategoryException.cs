ff123feb0e28d21b9c6c2b5fddfd274820b219a3
KamilPachuta <pachutakamil@gmail.com>
2025-09-24 22:13:54 +0200

    refactor(CurrencyAccounts): remove InvalidPaymentCategoryException as it is no longer needed

--- MoneyMinder/MoneyMinder.Domain/CurrencyAccounts/Exceptions/InvalidPaymentCategoryException.cs
+++
@@ -1,7 +0,0 @@
-﻿using MoneyMinder.Domain.Shared.Abstractions;
-using MoneyMinder.Domain.Shared.Enums;
-
-namespace MoneyMinder.Domain.CurrencyAccounts.Exceptions;
-
-internal sealed class InvalidPaymentCategoryException(Category category) 
-    : MoneyMinderException($"Transaction category: '{category}' is invalid.");
