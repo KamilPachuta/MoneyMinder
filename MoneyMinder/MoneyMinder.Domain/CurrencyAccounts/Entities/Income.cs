c86ff38b41da5d68466e223d2019a3d0e934056a
KamilPachuta <pachutakamil@gmail.com>
2025-09-24 18:09:24 +0200

    feat(CurrencyAccounts): add Income entity and NegativeAmountException for domain validation

---
+++ MoneyMinder/MoneyMinder.Domain/CurrencyAccounts/Entities/Income.cs
@@ -0,0 +1,28 @@
+﻿using MoneyMinder.Domain.CurrencyAccounts.Exceptions;
+using MoneyMinder.Domain.Shared.Enum;
+using MoneyMinder.Domain.Shared.Primitives;
+using MoneyMinder.Domain.Shared.ValueObjects;
+
+namespace MoneyMinder.Domain.CurrencyAccounts.Entities;
+
+public class Income : Transaction
+{
+    private Income()
+    {
+    }
+
+    public Income(TransactionName name, TransactionDate date, DefinedCurrency currency, Amount amount)
+    : base(name, date, currency,amount)
+    {
+        CheckAmount(amount);
+    }
+    
+    
+    private void CheckAmount(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            throw new NegativeAmountException(amount);
+        }
+    }
+}
---
+++ MoneyMinder/MoneyMinder.Domain/CurrencyAccounts/Exceptions/NegativeAmountException.cs
@@ -0,0 +1,6 @@
+﻿using MoneyMinder.Domain.Shared.Abstractions;
+
+namespace MoneyMinder.Domain.CurrencyAccounts.Exceptions;
+
+internal sealed class NegativeAmountException(decimal amount) 
+    : MoneyMinderException($"Amount: '{amount}' cannot be negative.");
