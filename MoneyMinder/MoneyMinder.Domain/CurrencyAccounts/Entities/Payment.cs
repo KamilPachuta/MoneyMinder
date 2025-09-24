bd510bd55e2804709e44ccf93efac16becdf5e4c
KamilPachuta <pachutakamil@gmail.com>
2025-09-24 18:09:08 +0200

    feat(CurrencyAccounts): add Payment entity and PositiveAmountException for domain validation

---
+++ MoneyMinder/MoneyMinder.Domain/CurrencyAccounts/Entities/Payment.cs
@@ -0,0 +1,28 @@
+﻿using MoneyMinder.Domain.CurrencyAccounts.Exceptions;
+using MoneyMinder.Domain.CurrencyAccounts.ValueObjects;
+using MoneyMinder.Domain.Shared.Enum;
+using MoneyMinder.Domain.Shared.Primitives;
+using MoneyMinder.Domain.Shared.ValueObjects;
+
+namespace MoneyMinder.Domain.CurrencyAccounts.Entities;
+
+public class Payment : Transaction
+{
+    public DefinedCategory Category { get; }
+    
+    public Payment(TransactionName name, TransactionDate date, DefinedCurrency currency, Amount amount, DefinedCategory? category) 
+        : base(name, date, currency,amount)
+    {
+        Category = category ?? Shared.Enum.Category.Other;
+        
+        CheckAmount(amount);
+    }
+    
+    private void CheckAmount(decimal amount)
+    {
+        if (amount >= 0)
+        {
+            throw new PositiveAmountException(amount);
+        }
+    }
+}
---
+++ MoneyMinder/MoneyMinder.Domain/CurrencyAccounts/Exceptions/PositiveAmountException.cs
@@ -0,0 +1,6 @@
+﻿using MoneyMinder.Domain.Shared.Abstractions;
+
+namespace MoneyMinder.Domain.CurrencyAccounts.Exceptions;
+
+internal sealed class PositiveAmountException(decimal amount)
+    : MoneyMinderException($"Payment amount: '{amount}' cannot be positive.");
