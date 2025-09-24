b1e50123ab3726965eabf0e0537cd954766e49d6
KamilPachuta <pachutakamil@gmail.com>
2025-09-24 18:00:57 +0200

    refactor(ValueObjects): rename TransactionAmount to Amount and update property name to Value

---
+++ MoneyMinder/MoneyMinder.Domain/Shared/ValueObjects/Amount.cs
@@ -0,0 +1,18 @@
+﻿namespace MoneyMinder.Domain.Shared.ValueObjects;
+
+public record Amount
+{
+    public decimal Value { get; }
+
+    public Amount(decimal amount)
+    {
+        Value = amount;
+    }
+    
+        
+    public static implicit operator decimal(Amount amount)
+        => amount.Value; 
+
+    public static implicit operator Amount(decimal amount)
+        => new(amount);
+}
