a1d29e3357b6417607722105f24aad1d137226c2
KamilPachuta <pachutakamil@gmail.com>
2025-09-24 18:11:48 +0200

    feat(CurrencyAccounts): add DuplicateLimitException and IncorrectLimitsCount for domain validation

---
+++ MoneyMinder/MoneyMinder.Domain/CurrencyAccounts/Exceptions/DuplicateLimitException.cs
@@ -0,0 +1,7 @@
+﻿using MoneyMinder.Domain.Shared.Abstractions;
+using MoneyMinder.Domain.Shared.Enum;
+
+namespace MoneyMinder.Domain.CurrencyAccounts.Exceptions;
+
+internal sealed class DuplicateLimitException(Category category) 
+    : MoneyMinderException($"Limit with category: '{category}' was already defined."); 
---
+++ MoneyMinder/MoneyMinder.Domain/CurrencyAccounts/Exceptions/IncorrectLimitsCount.cs
@@ -0,0 +1,5 @@
+﻿using MoneyMinder.Domain.Shared.Abstractions;
+
+namespace MoneyMinder.Domain.CurrencyAccounts.Exceptions;
+
+internal sealed class IncorrectLimitsCount() : MoneyMinderException("Limits must contain all categories");
