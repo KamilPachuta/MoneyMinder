7be37a7a999182a622ffc8a6cad9ffe9abd6b5be
KamilPachuta <pachutakamil@gmail.com>
2025-09-27 16:22:33 +0200

    feat(CurrencyAccounts): add DeleteCurrencyAccount command and handler

---
+++ MoneyMinder/MoneyMinder.Application/CurrencyAccounts/Commands/DeleteCurrencyAccountCommand.cs
@@ -0,0 +1,6 @@
+﻿using MoneyMinder.Application.CurrencyAccounts.Commands.Abstractions;
+
+namespace MoneyMinder.Application.CurrencyAccounts.Commands;
+
+public record DeleteCurrencyAccountCommand(Guid AccountId, Guid CurrencyAccountId) 
+    : CurrencyCommand(AccountId, CurrencyAccountId);
---
+++ MoneyMinder/MoneyMinder.Application/CurrencyAccounts/Commands/Handlers/DeleteCurrencyAccountHandler.cs
@@ -0,0 +1,20 @@
+﻿using MoneyMinder.Application.CurrencyAccounts.Commands.Abstractions;
+using MoneyMinder.Application.CurrencyAccounts.Services;
+using MoneyMinder.Domain.Repositories;
+
+namespace MoneyMinder.Application.CurrencyAccounts.Commands.Handlers;
+
+internal class DeleteCurrencyAccountHandler : CurrencyHandler<DeleteCurrencyAccountCommand>
+{
+    public DeleteCurrencyAccountHandler(ICurrencyAccountRepository repository, ICurrencyAccountReadService readService) 
+        : base(repository, readService)
+    {
+    }
+    
+    public override async Task Handle(DeleteCurrencyAccountCommand request, CancellationToken cancellationToken)
+    {
+        var currencyAccount = await GetCurrencyAccount(request);
+        
+        await _repository.DeleteAsync(currencyAccount);
+    }
+}
