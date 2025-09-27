fe6b9385d81e584bde89b971f42095d3c2ecf8ab
KamilPachuta <pachutakamil@gmail.com>
2025-09-27 16:52:34 +0200

    feat(SavingsAccounts): add read models for SavingsAccount and SavingsTransaction

---
+++ MoneyMinder/MoneyMinder.Infrastructure/EF/ReadModels/SavingsAccounts/SavingsAccountReadModel.cs
@@ -0,0 +1,29 @@
+﻿using MoneyMinder.Domain.Shared.Enums;
+
+namespace MoneyMinder.Infrastructure.EF.ReadModels.SavingsAccounts;
+
+public class SavingsAccountReadModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public Currency Currency { get; set; }
+    public decimal PlannedAmount { get; set; }
+    public decimal CurrentAmount { get; set; }
+
+    public Guid AccountId { get; set; }
+    
+    public IEnumerable<SavingsTransactionReadModel> Transactions { get; set; }
+
+    public SavingsAccountReadModel()
+    {
+    }
+    
+    public SavingsAccountReadModel(Guid id, string name, Currency currency, decimal plannedAmount, decimal currentAmount)
+    {
+        Id = id;
+        Name = name;
+        Currency = currency;
+        PlannedAmount = plannedAmount;
+        CurrentAmount = currentAmount;
+    }
+}
---
+++ MoneyMinder/MoneyMinder.Infrastructure/EF/ReadModels/SavingsAccounts/SavingsTransactionReadModel.cs
@@ -0,0 +1,31 @@
+﻿using MoneyMinder.Domain.SavingsAccounts.Enums;
+using MoneyMinder.Domain.Shared.Enums;
+
+namespace MoneyMinder.Infrastructure.EF.ReadModels.SavingsAccounts;
+
+public class SavingsTransactionReadModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public Currency Currency { get; set; }
+    public decimal Amount { get; set; }
+    public TransactionType Type { get; set; }
+
+    public Guid SavingsAccountId { get; set; }
+
+    public SavingsTransactionReadModel()
+    {
+    }
+    
+    public SavingsTransactionReadModel(Guid id, string name, DateTime date, Currency currency, decimal amount, TransactionType type)
+    {
+        Id = id;
+        Name = name;
+        Date = date;
+        Currency = currency;
+        Amount = amount;
+        Type = type;
+        
+    }
+}
