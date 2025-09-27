f6d1708a8be79219977e6cff23f4a949e85e9e75
KamilPachuta <pachutakamil@gmail.com>
2025-09-27 16:53:59 +0200

    feat(SavingsAccounts): add EF configurations for SavingsAccount and SavingsTransaction and read models

---
+++ MoneyMinder/MoneyMinder.Infrastructure/EF/Configuration/SavingsAccountConfiguration.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoneyMinder.Domain.SavingsAccounts;
+using MoneyMinder.Domain.SavingsAccounts.Entities;
+
+namespace MoneyMinder.Infrastructure.EF.Configuration;
+
+internal sealed class SavingsAccountConfiguration :
+    IEntityTypeConfiguration<SavingsAccount>,
+    IEntityTypeConfiguration<SavingsTransaction>
+{
+    public void Configure(EntityTypeBuilder<SavingsAccount> builder)
+    {
+        builder.ToTable(TableNames.SavingsAccounts);
+        builder.HasKey(sp => sp.Id);
+
+        builder
+            .Property(sa => sa.Name)
+            .HasConversion(n => n.Name, n => new(n))
+            .IsRequired();
+
+        builder
+            .Property(sa => sa.Currency)
+            .HasConversion(c => c.Currency, c => new(c))
+            .IsRequired();
+
+        builder
+            .Property(sa => sa.PlannedAmount)
+            .HasConversion(pa => pa.Value, pa => new(pa))
+            .IsRequired();
+        
+        builder
+            .Property(sa => sa.CurrentAmount)
+            .HasConversion(ca => ca.Value, ca => new(ca))
+            .IsRequired();
+
+        builder
+            .HasMany(sa => sa.Transactions)
+            .WithOne()
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+    
+    public void Configure(EntityTypeBuilder<SavingsTransaction> builder)
+    {
+        builder.ToTable(TableNames.SavingsTransactions);
+        builder.HasKey(st => st.Id);
+
+        builder
+            .Property(st => st.Name)
+            .HasConversion(n => n.Name, n => new(n))
+            .IsRequired();
+
+        builder
+            .Property(st => st.Date)
+            .HasConversion(td => td.Date, td => new(td))
+            .IsRequired();
+
+        builder
+            .Property(st => st.Currency)
+            .HasConversion(c => c.Currency, c => new(c))
+            .IsRequired();
+
+        builder
+            .Property(st => st.Amount)
+            .HasConversion(a => a.Value, a => new(a))
+            .IsRequired();
+
+        builder
+            .Property(st => st.Type)
+            .HasConversion(tt => tt.TransactionType, tt => new(tt))
+            .IsRequired();
+    }
+}
---
+++ MoneyMinder/MoneyMinder.Infrastructure/EF/Configuration/SavingsAccountReadConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoneyMinder.Infrastructure.EF.ReadModels.SavingsAccounts;
+
+namespace MoneyMinder.Infrastructure.EF.Configuration;
+
+public class SavingsAccountReadConfiguration:
+    IEntityTypeConfiguration<SavingsAccountReadModel>,
+    IEntityTypeConfiguration<SavingsTransactionReadModel>
+{
+    public void Configure(EntityTypeBuilder<SavingsAccountReadModel> builder)
+    {
+        builder.ToTable(TableNames.SavingsAccounts);
+        builder.HasKey(sp => sp.Id);
+
+        builder
+            .Property(sp => sp.Name);
+
+        builder
+            .Property(sp => sp.Currency);
+
+        builder
+            .Property(sp => sp.PlannedAmount);
+
+        builder
+            .Property(sp => sp.CurrentAmount);
+
+        builder
+            .HasMany(sp => sp.Transactions)
+            .WithOne()
+            .HasForeignKey(st => st.SavingsAccountId);
+    }
+    
+    public void Configure(EntityTypeBuilder<SavingsTransactionReadModel> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
