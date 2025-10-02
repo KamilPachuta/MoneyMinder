c32d2f6db280703a35ed1351be7f8e239add91c4
KamilPachuta <pachutakamil@gmail.com>
2025-10-02 23:21:55 +0200

    refactor: introduce `BasePage` for shared dialog and notification logic, update NavMenu and dialog components accordingly

---
+++ MoneyMinderClient/MoneyMinderClient/Core/BasePage.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace MoneyMinderClient.Core;
+
+public class BasePage : ComponentBase
+{
+    [Inject] protected IDialogService DialogService { get; set; }
+    [Inject] protected ISnackbar Snackbar { get; set; }
+    [Inject] protected NavigationManager NavigationManager { get; set; }
+    
+    protected async Task ShowDialogAsync<TDialog>()
+        where TDialog : ComponentBase
+    {
+        var dialog = await DialogService.ShowAsync<TDialog>();
+        var result = await dialog.Result;
+
+        if (result.Canceled)
+        {
+            Snackbar.Add("Operation canceled", Severity.Info);
+            return;
+        }
+
+        var dialogResult = await dialog.GetReturnValueAsync<Result>();
+
+        if (dialogResult.Succeeded)
+        {
+            Snackbar.Add("Operation successful", Severity.Success);
+            NavigationManager.Refresh();
+        }
+        else
+        {
+            Snackbar.Add("Operation failed", Severity.Error);
+            foreach (var error in dialogResult.ErrorList)
+                Snackbar.Add(error, Severity.Error);
+        }
+    }
+}
