ceae86c73be5ebbed382936e856e12246319a6e5
KamilPachuta <pachutakamil@gmail.com>
2025-10-02 21:20:45 +0200

    feat(Accounts): add `SavingsAccountService` with `GetSavingsAccountNames` query and interface implementation

---
+++ MoneyMinderClient/MoneyMinderClient/Services/Interfaces/ISavingsAccountService.cs
@@ -0,0 +1,9 @@
+﻿using MoneyMinderClient.Core;
+using MoneyMinderContracts.Responses.SavingsAccounts;
+
+namespace MoneyMinderClient.Services.Interfaces;
+
+public interface ISavingsAccountService
+{
+    public Task<Result<GetSavingsAccountNamesResponse>> GetSavingsAccountNames();
+}
---
+++ MoneyMinderClient/MoneyMinderClient/Services/SavingsAccountService.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json;
+using MoneyMinderClient.Core;
+using MoneyMinderClient.Services.Interfaces;
+using MoneyMinderContracts.Responses.SavingsAccounts;
+
+namespace MoneyMinderClient.Services;
+
+public class SavingsAccountService : ISavingsAccountService
+{
+    private readonly HttpClient _httpClient;
+
+    public SavingsAccountService(IHttpClientFactory httpClientFactory)
+    {
+        _httpClient = httpClientFactory.CreateClient("Auth");
+    }
+    
+    #region Commands
+    
+    #endregion
+    
+    #region Queries
+    
+    public async Task<Result<GetSavingsAccountNamesResponse>> GetSavingsAccountNames()
+    {
+        var responseMessage = await _httpClient.GetAsync("api/SavingsAccount/Names");
+        var content = await responseMessage.Content.ReadAsStringAsync();
+
+        if (!responseMessage.IsSuccessStatusCode)
+        {
+            return Result<GetSavingsAccountNamesResponse>.Failure(
+                $"Status Code: {responseMessage.StatusCode}",
+                $"Content: {content}"
+            );
+        }
+
+        var response = JsonSerializer.Deserialize<GetSavingsAccountNamesResponse>(content, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        });
+
+        if (response == null)
+        {
+            return Result<GetSavingsAccountNamesResponse>.Failure(
+                "Response is null.",
+                $"Status Code: {responseMessage.StatusCode}",
+                $"Content: {content}"
+            );
+        }
+
+        return Result<GetSavingsAccountNamesResponse>.Success(response);
+    }
+    
+    #endregion
+
+}
