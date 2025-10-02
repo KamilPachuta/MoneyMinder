af57f7e596fecae65291ae5fab46aafa8d4d23d8
KamilPachuta <pachutakamil@gmail.com>
2025-10-02 21:18:19 +0200

    refactor(Validation): unify validation logic with generic `ValidationAdapter`, remove `LoginAccountValidationAdapter`

---
+++ MoneyMinderClient/MoneyMinderClient/Core/ValidationAdapter.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace MoneyMinderClient.Core;
+
+public static class ValidationAdapter
+{
+    public static Func<object, string, Task<IEnumerable<string>>> ValidateValue<TRequest>(IValidator<TRequest> validator)
+        where TRequest : class
+    {
+        return async (model, propertyName) =>
+        {
+            var context = ValidationContext<TRequest>.CreateWithOptions(
+                (TRequest)model,
+                x => x.IncludeProperties(propertyName)
+            );
+
+            var result = await validator.ValidateAsync(context);
+            return result.IsValid
+                ? Array.Empty<string>()
+                : result.Errors.Select(e => e.ErrorMessage);
+        };
+    }
+}
