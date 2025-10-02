c52e5a1383ac3596344f8befd559e1664537a5bb
KamilPachuta <pachutakamil@gmail.com>
2025-10-02 21:18:56 +0200

    feat(Client): introduce `BearerHandler`, configure HTTP client with token support, and set default culture to `en-US`

---
+++ MoneyMinderClient/MoneyMinderClient/BearerHandler.cs
@@ -0,0 +1,25 @@
+﻿using Blazored.LocalStorage;
+
+namespace MoneyMinderClient;
+
+public class BearerHandler : DelegatingHandler
+{
+    private readonly ILocalStorageService _localStorageService;
+    
+    public BearerHandler(ILocalStorageService localStorageService)
+    {
+        _localStorageService = localStorageService;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var token = await _localStorageService.GetItemAsStringAsync("Token");
+
+        if (!string.IsNullOrWhiteSpace(token))
+        {
+            request.Headers.Add("Authorization", $"Bearer {token}");
+        }
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
--- MoneyMinderClient/MoneyMinderClient/Program.cs
+++ MoneyMinderClient/MoneyMinderClient/Program.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
@@ -12,12 +13,28 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]) });
+
+var culture = new CultureInfo("en-US");
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped<BearerHandler>();
+
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]) });
+builder.Services.AddHttpClient("Auth", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"]);
+}).AddHttpMessageHandler<BearerHandler>();
+
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<ICurrencyAccountService, CurrencyAccountService>();
+builder.Services.AddScoped<ISavingsAccountService, SavingsAccountService>();
 builder.Services.AddScoped<AppAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(sp => sp.GetRequiredService<AppAuthenticationStateProvider>());
-builder.Services.AddMudServices();
 builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
