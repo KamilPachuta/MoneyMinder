0daf614fb63e55935bc546b285b7b4ce94c54314
KamilPachuta <pachutakamil@gmail.com>
2025-10-05 15:21:23 +0200

    feat(DTOs): add `BalanceDto` and `CurrencyTransactionDto` for representing balances and currency transactions

---
+++ MoneyMinderContracts/MoneyMinderContracts/Models/Dtos/BalanceDto.cs
@@ -0,0 +1,9 @@
+﻿using MoneyMinderContracts.Models.Enums;
+
+namespace MoneyMinderContracts.Models.Dtos;
+
+public class BalanceDto
+{
+    public CurrencyDto Currency { get; set; }
+    public decimal Amount { get; set; }
+}
---
+++ MoneyMinderContracts/MoneyMinderContracts/Models/Dtos/CurrencyTransactionDto.cs
@@ -0,0 +1,14 @@
+﻿using MoneyMinderContracts.Models.Enums;
+
+namespace MoneyMinderContracts.Models.Dtos;
+
+public class CurrencyTransactionDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public CurrencyDto Currency { get; set; }
+    public decimal Amount { get; set; }
+    public CategoryDto? Category { get; set; }
+    
+}
