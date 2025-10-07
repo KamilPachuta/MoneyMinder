73b706acebe00552f39f2d3dd97685c3f8a54a81
KamilPachuta <pachutakamil@gmail.com>
2025-10-07 17:50:26 +0200

    refactor(Core/UI): centralize path building logic in `PathBuilder` and update `NavMenu` accordingly

---
+++ MoneyMinderClient/MoneyMinderClient/Core/PathBuilder.cs
@@ -0,0 +1,14 @@
+﻿namespace MoneyMinderClient.Core;
+
+  
+
+public static class PathBuilder
+{
+    public static string CurrencyAcountPath(string name) => $"/CurrencyAccount/{name}";
+
+    public static string CurrencyTransactionHistoryPath(string name) => $"/CurrencyAccount/{name}/TransactionHistory";
+            
+    public static string BudgetPath(string name) => $"/CurrencyAccount/{name}/Budget";
+    
+    public static string SavingsAccountPath(string name) => $"/SavingsAccount/{name}";
+}
