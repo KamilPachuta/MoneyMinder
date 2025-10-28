345698294fd099c4495e6c73b47c7479d147062e
KamilPachuta <pachutakamil@gmail.com>
2025-10-28 19:54:46 +0100

    feat(UI/BasePage): add centralized error handling with Snackbar notifications

--- MoneyMinderClient/MoneyMinderClient/Core/BasePage.cs
+++ MoneyMinderClient/MoneyMinderClient/Core/BasePage.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using Microsoft.AspNetCore.Components;
 using MudBlazor;
 
 namespace MoneyMinderClient.Core;
@@ -9,6 +11,46 @@ public class BasePage : ComponentBase
     [Inject] protected ISnackbar Snackbar { get; set; }
     [Inject] protected NavigationManager NavigationManager { get; set; }
     
+    private ObservableCollection<string> _errors = new();
+
+    protected override void OnInitialized()
+    {
+        _errors.CollectionChanged += Errors_CollectionChanged;
+    }
+    
+    protected void SetErrors(IEnumerable<string> errors)
+    {
+        _errors.Clear();
+        foreach (var err in errors)
+            _errors.Add(err);
+    }
+    protected void SetError(string error)
+    {
+        _errors.Clear();
+        if (!string.IsNullOrWhiteSpace(error)) 
+            _errors.Add(error);
+    }
+ 
+    
+    private void Errors_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.Action == NotifyCollectionChangedAction.Add && e.NewItems is not null)
+        {
+            foreach (var item in e.NewItems)
+            {
+                var msg = item?.ToString();
+                if (!string.IsNullOrWhiteSpace(msg))
+                {
+                    InvokeAsync(() =>
+                    {
+                        Snackbar.Configuration.PositionClass = Defaults.Classes.Position.BottomRight;
+                        Snackbar.Configuration.SnackbarVariant = Variant.Filled;
+                        Snackbar.Add(msg, Severity.Error);
+                    });
+                }
+            }
+        }
+    }
     protected async Task ShowDialogAsync<TDialog>(string title ,DialogParameters? parameters = null)
         where TDialog : ComponentBase
     {
