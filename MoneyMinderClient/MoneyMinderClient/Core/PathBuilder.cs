e3cf7de23135c45ffbafde130e7f8529ab7c73cf
kamil.pachuta <kamil.pachuta@bpx.pl>
2026-01-13 09:20:07 +0100

    feat(UI): Transactions History merged into Currency Account page.

--- MoneyMinderClient/MoneyMinderClient/Core/PathBuilder.cs
+++ MoneyMinderClient/MoneyMinderClient/Core/PathBuilder.cs
@@ -5,8 +5,6 @@
 public static class PathBuilder
 {
     public static string CurrencyAcountPath(string name) => $"/CurrencyAccount/{name}";
-
-    public static string CurrencyTransactionHistoryPath(string name) => $"/CurrencyAccount/{name}/TransactionHistory";
             
     public static string BudgetPath(string name) => $"/CurrencyAccount/{name}/Budget";
     
